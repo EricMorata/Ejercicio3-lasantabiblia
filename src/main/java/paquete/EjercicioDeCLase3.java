6f56a3e16c2455a5ac5778f3df696f0315b06a7a
Angel17121984 <checa_angel@hotmail.com>
2021-10-15 19:33:44 +0200

    escrito línea 7
    entrando al turrón con la historia

--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -4,7 +4,7 @@ public class EjercicioDeCLase3 {
 	public static void main(String[] args) {
 		System.out.println(" Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
 		System.out.println(" el número era del malvado profesor de programación y lo llamó");
-	
+		System.out.println(" el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
 	
 	
 	
