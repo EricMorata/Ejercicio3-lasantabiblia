a456e306a729410f286c3681696c6cb44a5055f4
Angel17121984 <checa_angel@hotmail.com>
2021-10-15 19:42:56 +0200

    Continuación línea 9 historia+comentario
    editado en línea 9 una línea de la historia junto con un comentario para cuando os bajéis esta versión. Es solo un recordatorio el comentario.

--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -6,7 +6,7 @@ public class EjercicioDeCLase3 {
 		System.out.println(" el número era del malvado profesor de programación y lo llamó");
 		System.out.println(" el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
 		System.out.println(" pero para eso tenía que reunirse en la clase, todavía sin montar, con el resto de sus compañeros");
-	
-	
+		System.out.println("cogió el autobús y mientras se rascaba el ojete del culo, se preguntaba, ¿de qué tratará el juego?"); //bueno chicos, he escrito las 5 primeras líneas de la historia, subido por separado para que os salgan varías versiones. OS recuerdo que son 10 por persona, pondré otras 5 el domingo por donde lo hayais dejado. Por cierto la historia es una chorrada, poned coñas y para delante.
+			
 	}
 }
