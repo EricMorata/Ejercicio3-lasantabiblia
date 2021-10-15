30fc183c5ca82a12f7b09652aa63891d28779523
Angel17121984 <checa_angel@hotmail.com>
2021-10-15 19:30:42 +0200

    continuación comienzo de la historia
    continuación en línea 6

--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -3,5 +3,10 @@ package paquete;
 public class EjercicioDeCLase3 {
 	public static void main(String[] args) {
 		System.out.println(" Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
+		System.out.println(" el número era del malvado profesor de programación y lo llamó");
+	
+	
+	
+	
 	}
 }
