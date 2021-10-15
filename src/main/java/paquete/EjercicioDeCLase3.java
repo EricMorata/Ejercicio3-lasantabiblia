a3e4ef7c34e22be747b22c2d019b5d80f631793d
Angel17121984 <checa_angel@hotmail.com>
2021-10-15 19:36:53 +0200

    línea 8
    la historia se desarrolla antes de que se forme el "nudo argumental"

--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -5,7 +5,7 @@ public class EjercicioDeCLase3 {
 		System.out.println(" Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
 		System.out.println(" el número era del malvado profesor de programación y lo llamó");
 		System.out.println(" el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
-	
+		System.out.println(" pero para eso tenía que reunirse en la clase, todavía sin montar, con el resto de sus compañeros");
 	
 	
 	}
