09658105f402beed26cdfe37ae5e0bf881619d75
NsoEnigma <alexcelia910@gmail.com>
2021-10-16 13:36:21 +0200

    linea 10
    continuacion de la historia

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# EJERCICIO3_LASANTABIBLIA
+ ejercicio numero 3 1dam Eric, Alex, Dani, Angel
--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -2,11 +2,12 @@ package paquete;
 
 public class EjercicioDeCLase3 {
 	public static void main(String[] args) {
-		System.out.println(" Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
-		System.out.println(" el número era del malvado profesor de programación y lo llamó");
-		System.out.println(" el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
-		System.out.println(" pero para eso tenía que reunirse en la clase, todavía sin montar, con el resto de sus compañeros");
+		System.out.println("Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
+		System.out.println("el número era del malvado profesor de programación y lo llamó");
+		System.out.println("el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
+		System.out.println("pero para eso tenía que reunirse en la clase, todavía sin montar, con el resto de sus compañeros");
 		System.out.println("cogió el autobús y mientras se rascaba el ojete del culo, se preguntaba, ¿de qué tratará el juego?"); //bueno chicos, he escrito las 5 primeras líneas de la historia, subido por separado para que os salgan varías versiones. OS recuerdo que son 10 por persona, pondré otras 5 el domingo por donde lo hayais dejado. Por cierto la historia es una chorrada, poned coñas y para delante.
-			
+		System.out.println("Justo en mitad del viaje empezo a salir un gas por el ventilador y me quede frito (la verdad es que me vino de puta madre)");
+		
 	}
 }
