b8bb48bff7057bfd3fb579bdc69c1cbb13d6cb14
EricMorata <ericoensis@gmail.com>
2021-10-18 14:26:05 +0200

    Arreglo conflicto
    He solucionado el conflicto ocurrido con todas las lineas

--- src/main/java/paquete/EjercicioDeCLase3.java
+++ src/main/java/paquete/EjercicioDeCLase3.java
@@ -2,41 +2,48 @@ package paquete;
 
 public class EjercicioDeCLase3 {
 	public static void main(String[] args) {
-		System.out.println("Un d�a un alumno de CENEC se despert� con una tarjeta dorada con s�mbolos en el anverso y un tel�fono en el reverso");
-		System.out.println("el n�mero era del malvado profesor de programaci�n y lo llam�");
-		System.out.println("el profesor apareci� en una v�deollamada con una m�scara de caballo y le propuso al inocente alumno un juego");
-		System.out.println("pero para eso ten�a que reunirse en la clase, todav�a sin montar, con el resto de sus compa�eros");
-		System.out.println("cogi� el autob�s y mientras se rascaba el ojete del culo, se preguntaba, �de qu� tratar� el juego?"); //bueno chicos, he escrito las 5 primeras l�neas de la historia, subido por separado para que os salgan var�as versiones. OS recuerdo que son 10 por persona, pondr� otras 5 el domingo por donde lo hayais dejado. Por cierto la historia es una chorrada, poned co�as y para delante.
+		System.out.println("Un día un alumno de CENEC se despertó con una tarjeta dorada con símbolos en el anverso y un teléfono en el reverso");
+		System.out.println("el número era del malvado profesor de programación y lo llamó");
+		System.out.println("el profesor apareció en una vídeollamada con una máscara de caballo y le propuso al inocente alumno un juego");
+		System.out.println("pero para eso tenía que reunirse en la clase, todavía sin montar, con el resto de sus compañeros");
+		System.out.println("cogió el autobús y mientras se rascaba el ojete del culo, se preguntaba, ¿de qué tratará el juego?"); //bueno chicos, he escrito las 5 primeras líneas de la historia, subido por separado para que os salgan varías versiones. OS recuerdo que son 10 por persona, pondré otras 5 el domingo por donde lo hayais dejado. Por cierto la historia es una chorrada, poned coñas y para delante.
 		System.out.println("Justo en mitad del viaje empezo a salir un gas por el ventilador y me quede frito (la verdad es que me vino de puta madre)");
-		System.out.println("al despertarme me encontre con todos los demas compa�eros de CENEC y empezamos a preguntarnos que pasaba");
-		System.out.println("cuando empezaba a montarse escanlado empezaron a entrar unas personas con caretas y pollos de juguete y justo detras estaba �l, el maestro de programacion con su careta de caballo");
+		System.out.println("al despertarme me encontre con todos los demas compañeros de CENEC y empezamos a preguntarnos que pasaba");
+		System.out.println("cuando empezaba a montarse escanlado empezaron a entrar unas personas con caretas y pollos de juguete y justo detras estaba él, el maestro de programacion con su careta de caballo");
 		System.out.println("el maestro empezo a explicarnos que es lo que pasaba, que nos habia llevado ahi para conseguir aprobados de programacion por estar todos suspensos");
 		System.out.println("queria que jugaramos a varios juegos desesperantes con los que entraban ganas de lanzar el ordenador por la ventana");
 		System.out.println("y nosotros creyendo que ya habiamos tenido suficientes putos jueguecitos despues de estar 2 horas con cada nivel, lo que nos espera...");
-		System.out.println("con el primer juego nos queria poner a ver 3 videos de 2 horas seguidas, quien nos las aguantara tendr�a un horroroso final que no nos quiso desvelar");
+		System.out.println("con el primer juego nos queria poner a ver 3 videos de 2 horas seguidas, quien nos las aguantara tendría un horroroso final que no nos quiso desvelar");
 		System.out.println("Al principio todo iba bien... Cada uno con su ordenador comiendose unos nachos, aunque a la mitad del tiempo empezo a joderse todo");
 		System.out.println("Uno empezo a descontrolarse y a decir que no iba a ver ni un puto video mas sin que le trajeran una putisima cerveza que estaba sediento");
-		System.out.println("Cuando le dijeron que no apago el ordenador y los profesores enmascarados fueron con sus pollos y le dieron una paliza hasta dejarlo muerto"); //bueno un poco loco pero aqui estan msi 10 l�neas, si quereis cambiar algo no dudeis en hacerlo ;) (alejandro)
-		System.out.println("Nos quedamos ojipl�ticos cuando aparecieron 4 enanos con un ataud adornado como una caja de regalos, todos ellos disfrazado como el cabronazo de SAW");
-		System.out.println("a los 7 minutos, otros dos compa�eros empezaron a tener ataques epil�pticos....se caen al suelo, bailaron breakdance y espicharon");
-		System.out.println("ya solo quedabamos 27 inocentes alumnos sometidos a tercer v�deo del malvado profesor de programaci�n cuando sin venir a cuento....");
-		System.out.println(" se fue la luz, cosa ya habitual y como ya hab�an ca�do 3 de nuestros compa�eros, con la visi�n de Predator de nuestro profesor nos anunci�...");
-		System.out.println("� queda finalizado el primer juego, salgan 10 minutos a descansar y a la vuelta les revelaremos el segundo juego!. Nos levantamos y salimos como pollos sin cabeza de aquel siniestro lugar"); // chicos con esto ya ser�a mi d�cima l�nea(�ngel), espero que nadie est� escribiendo a la vez que yo o entramos en conflicto, en ese caso, que prevalezca el de ambas por favor. Buen resto de finde.
-		System.out.println("Una vez finalizados esos 10 minutos en los cuales todos nos mirabamos pensando en quien ser�a el siguiente en caer, nos dirigimos de nuevo a esas terribles instalaciones");//comenzando el desenlace de la historia (Eric)
-		System.out.println("ya dentros de esas instalaciones, vinieron unos seres de otra galaxia a colocar paneles y encerrarnos a�n m�s en ese sitio el cual ya ol�a a chotocabra");
-		System.out.println("el profesor centauro se diriji� a nosotros y nos propuso el segundo juego, que consistia en descifrar un lenguaje venido de alg�n rinc�n de este mundo al cual lo llamaban JAVA");
-		System.out.println("este juego trajo conflictos a la vez que uni� a los participantes del curso ya que que si uno no avanzaba en la comprensi�n, el resto morir�a de una forma un tanto singular; a pellizcos");
-		System.out.println("tras la muerte de todos los alumnos, ya que los conflictos lo arreglaron a pellizcos, quedamos 4 alumnos y el profesor centauro, y solo me queda decir de una forma que JAVA no lo entender�,@policia P L E H!!!"); // hasta aqu� mi aportaci�n, espero que todos lo hagais antes de la fecha de entrega (Eric)
-		System.out.println("El profesor mand� un nuevo video pero este video era algo turbio porque si entrabas en el no pod�as salirte");
-		System.out.println("en este video el maestro no ten�a nig�n tipo de m�scara si no que ten�a un disfraz (a mi parecer un poco feo y horrible)");
-		System.out.println("El disfraz que ten�a puesto era un disfraz de medusa (la fea de las serpientes) y todos nos reiamos de �l, pero no sabiamos que algo malo iba a pasar...");
-		System.out.println("ese disfraz hac�a lo mismo que la cabeza de medusa, si le mirabas a los ojos te dejaba petrificado, nosotros pensabamos que era una broma pero nos dimos cuenta de que no cuando...");
-		System.out.println("uno de nosotros lo mir� y se convirti� en piedra, una vez vimos eso salimos corriendo, ya solo quedabamos 3 y antes de irnos el profesor nos dijo que ten�amos que arreglar un error si quieramos que se quitara el disfraz");
-		System.out.println("Una vez fuera de la clase nos fuimos a nuestra casa, teniamos que buscar una solucion al problema para no quedarnos como nuestro compa�ero");
+		System.out.println("Cuando le dijeron que no apago el ordenador y los profesores enmascarados fueron con sus pollos y le dieron una paliza hasta dejarlo muerto"); //bueno un poco loco pero aqui estan msi 10 lï¿½neas, si quereis cambiar algo no dudeis en hacerlo ;) (alejandro)
+		System.out.println("Nos quedamos ojiplï¿½ticos cuando aparecieron 4 enanos con un ataud adornado como una caja de regalos, todos ellos disfrazado como el cabronazo de SAW");
+		System.out.println("a los 7 minutos, otros dos compaï¿½eros empezaron a tener ataques epilï¿½pticos....se caen al suelo, bailaron breakdance y espicharon");
+		System.out.println("ya solo quedabamos 27 inocentes alumnos sometidos a tercer vï¿½deo del malvado profesor de programaciï¿½n cuando sin venir a cuento....");
+		System.out.println(" se fue la luz, cosa ya habitual y como ya habï¿½an caï¿½do 3 de nuestros compaï¿½eros, con la visiï¿½n de Predator de nuestro profesor nos anunciï¿½...");
+		System.out.println("ï¿½ queda finalizado el primer juego, salgan 10 minutos a descansar y a la vuelta les revelaremos el segundo juego!. Nos levantamos y salimos como pollos sin cabeza de aquel siniestro lugar"); // chicos con esto ya serï¿½a mi dï¿½cima lï¿½nea(ï¿½ngel), espero que nadie estï¿½ escribiendo a la vez que yo o entramos en conflicto, en ese caso, que prevalezca el de ambas por favor. Buen resto de finde.
+		System.out.println("Una vez finalizados esos 10 minutos en los cuales todos nos mirabamos pensando en quien serï¿½a el siguiente en caer, nos dirigimos de nuevo a esas terribles instalaciones");//comenzando el desenlace de la historia (Eric)
+		System.out.println("ya dentros de esas instalaciones, vinieron unos seres de otra galaxia a colocar paneles y encerrarnos aï¿½n mï¿½s en ese sitio el cual ya olï¿½a a chotocabra");
+		System.out.println("el profesor centauro se dirijiï¿½ a nosotros y nos propuso el segundo juego, que consistia en descifrar un lenguaje venido de algï¿½n rincï¿½n de este mundo al cual lo llamaban JAVA");
+		System.out.println("este juego trajo conflictos a la vez que uniï¿½ a los participantes del curso ya que que si uno no avanzaba en la comprensiï¿½n, el resto morirï¿½a de una forma un tanto singular; a pellizcos");
+		System.out.println("tras la muerte de todos los alumnos, ya que los conflictos lo arreglaron a pellizcos, .quedamos 5 alumnos y el profesor centauro");
+		System.out.println("estos 5 alumnos prosiguieron aprendiendo el lenguaje JAVA y a usar repositorios sin llegar a conflictos ");
+		System.out.println("pero todo iba bien hasta que el profesor centauro mand� el siguiente juego: programar en una gameboycolor una app de citas");
+		System.out.println("era ronda eliminatoria y nos daba de plazo 2 d�as laborales que en el espacio/tiempo de cenec eran 3 horas");
+		System.out.println("los alumnos Eric, Alex, Angel y Dani consiguieron clasificarse y avocaron a x a la muerte, siendo ellos mismo quien tendrian que matarlo");
+		System.out.println("Eric saco de su espalda una espada laser 3000 y rebano el codo de daniel, alex saco un dildo y empalo a x, provocandole un desgarro y Angel con el arco de un violin degollo a x y despues toc� la cancion de tengo un tractor amarillo");
+		System.out.println("tras este escalabroso asunto todos siguieron como si nada pasase, esperando las indicaciones del profesor centauro");//hasta aqu� mi aportaci�n ERIC
+		System.out.println("El profesor mandó un nuevo video pero este video era algo turbio porque si entrabas en el no podías salirte");
+		System.out.println("en este video el maestro no tenía nigún tipo de máscara si no que tenía un disfraz (a mi parecer un poco feo y horrible)");
+		System.out.println("El disfraz que tenía puesto era un disfraz de medusa (la fea de las serpientes) y todos nos reiamos de él, pero no sabiamos que algo malo iba a pasar...");
+		System.out.println("ese disfraz hacía lo mismo que la cabeza de medusa, si le mirabas a los ojos te dejaba petrificado, nosotros pensabamos que era una broma pero nos dimos cuenta de que no cuando...");
+		System.out.println("uno de nosotros lo miró y se convirtió en piedra, una vez vimos eso salimos corriendo, ya solo quedabamos 3 y antes de irnos el profesor nos dijo que teníamos que arreglar un error si quieramos que se quitara el disfraz");
+		System.out.println("Una vez fuera de la clase nos fuimos a nuestra casa, teniamos que buscar una solucion al problema para no quedarnos como nuestro compañero");
 		System.out.println("Cada uno en su casa tenia que buscar porque ese disfraz que tenia funcionaba como la cabeza real de medusa (porque era una fumada), buscabamos y buscabamos y no encontramos nada hasta que...");
 		System.out.println("Encontramos en una pagina de la deep web que se podia comprar una cabeza de medusa pero valia un pastizal, asi que hablamos entre todos para ir a robarla");
 		System.out.println("La cabeza estaba escondida en unos almacenes en un poligono, asi que fuimos los 3 a robarla, habia muchos guardias cuidando el almacen asi que teniamos que distraerlos de alguna forma");
 		System.out.println("uno de nosotros tenia que ser el cebo de los guardias para que los otros pudiesen cogerla, asi que me puse un disfraz de moro vendemalacatones y me puse a correr al lado de ellos para que me sigueran");
 		System.out.println("Ahora con los guardias fuera, los otros 2 entraron y cojieron la cabeza, fueron a la clase con la cabeza y miraron a la cabeza del maestro y se neutralizaron las medusas, ellos 2 corrijieron el error y se fueron");//hasta aqui mis 10 lineas, no es recomendable leer, es una fumada xd, Daniel :p
+		
 	}
 }
